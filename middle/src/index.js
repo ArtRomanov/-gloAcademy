01f453ff141f51fb092171a324a9df732e058ab7
Artur Romanov <romanovjg@yandex.ru>
2020-09-24 15:51:37 +0300

    slider added

--- middle/src/index.js
+++ middle/src/index.js
@@ -1,6 +1,9 @@
 import buttonsEventListeners from './modules/buttons';
 import sendForm from './modules/sendForm';
+import slider from './modules/slider';
 
 buttonsEventListeners();
 
 sendForm();
+
+slider();
