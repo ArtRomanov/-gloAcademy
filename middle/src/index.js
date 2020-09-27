8df741126900730d491b4275b197bed18c337667
Artur Romanov <romanovjg@yandex.ru>
2020-09-27 20:10:19 +0300

    slider for services v0.0

--- middle/src/index.js
+++ middle/src/index.js
@@ -1,7 +1,8 @@
 import buttonsEventListeners from './modules/buttons';
 import sendForm from './modules/sendForm';
-import slider from './modules/slider';
+import slider from './modules/sliderAuto';
 import calcFunc from './modules/calc';
+import SliderCarousel from './modules/sliderServices';
 
 buttonsEventListeners();
 
@@ -10,3 +11,29 @@ sendForm();
 slider();
 
 calcFunc();
+
+const newOne = new SliderCarousel({
+    main: '.super_wrapper',
+    wrap: '.services-slider',
+    prev: '#arrow-left',
+    next: '#arrow-right',
+    infinity: true,
+    // настройки для аддаптивности
+    responsive: [{
+        breakpoint: 1280,
+        slideToShow: 4
+    },
+    {
+        breakpoint: 1024,
+        slideToShow: 3
+    },
+    {
+        breakpoint: 768,
+        slideToShow: 2
+    },
+    {
+        breakpoint: 576,
+        slideToShow: 1
+    }]
+});
+newOne.init();
---
+++ middle/src/modules/sliderAuto.js
@@ -0,0 +1,27 @@
+const slider = () => {
+    const sliderBlock = document.querySelector('.main-slider'),
+        slides = sliderBlock.querySelectorAll('.slide');
+    let currentSlide = 0;
+
+    const prevSlide = (elem, index) => {
+        elem[index].style.display = 'none';
+    };
+    const nextSlide = (elem, index) => {
+        elem[index].style.display = 'block';
+        elem[index].style.display = '-webkit-box';
+    };
+
+
+    const autoPlaySlide = () => {
+        prevSlide(slides, currentSlide);
+        currentSlide++;
+        if (currentSlide >= slides.length) {
+            currentSlide = 0;
+        }
+        nextSlide(slides, currentSlide);
+    };
+
+    setInterval(autoPlaySlide, 2000);
+};
+
+export default slider;
